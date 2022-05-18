2b1b64352e04027419d4b805895f281df2a4411a
Imran Hossen <imranhossen20@gmail.com>
2022-05-18 16:01:16 +0600

    first commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home page</h1>
+            <button class="btn btn-primary">Button</button>
+        </div>
+    );
+};
+
+export default Home;
